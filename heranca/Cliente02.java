ba72e7bd0517f651883624bb84e9b4d3bc1e8f4c
Thiago Fonseca <104439109+ThiagooFonseca@users.noreply.github.com>
2022-05-17 10:24:40 -0300

    Add files via upload

---
+++ heranca/Cliente02.java
@@ -0,0 +1,20 @@
+package Cliente02;
+
+/*
+ 1) Crie uma classe cliente e apresente os atributos e métodos referentes esta classe,
+  em seguida crie um objeto cliente, defina as instancias deste objeto
+   e apresente as informações deste objeto no console.
+ */
+
+ {
+
+	public static void main(String[] args) {
+		
+		Cliente cliente = Cliente.cadastraCliente();
+		cliente.toString();
+
+		
+		
+		
+	}
+}
